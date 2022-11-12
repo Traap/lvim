269927cb245a038b26c7369912a99bb2f5786edf
Traap <gary.a.howard@mac.com>
2022-11-12 12:33:24 -0800

    Convert global variable to local variable.

--- lua/traap/zymurgy.lua
+++ lua/traap/zymurgy.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd('ColorScheme', { command = 'highlight clear Folded' 
 -- See
 -- https://mitchellt.com/2022/05/15/WSL-Neovim-Lua-and-the-Windows-Clipboard.html
 
-in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+local in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
 
 if in_wsl then
     vim.g.clipboard = {
