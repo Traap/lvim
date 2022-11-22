48c30815ab4aa0820e3c15aea9b927381bfebb16
Traap <gary.a.howard@mac.com>
2022-11-22 08:48:49 -0800

    Begin setting up DAP.

--- lua/traap/zymurgy.lua
+++ lua/traap/zymurgy.lua
@@ -2,4 +2,8 @@
 
 -- Experimental configuration items.
 
+require('nvim-dap-virtual-text').setup()
+require('dap-go').setup()
+require('dapui').setup()
+
 -- ------------------------------------------------------------------------- }}}
