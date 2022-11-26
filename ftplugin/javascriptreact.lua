a808763c8db88b63fa76ae983b014a53b6e337e4
Traap <gary.a.howard@mac.com>
2022-11-26 00:03:56 -0800

    Complete movement to constants.lua.

--- ftplugin/javascriptreact.lua
+++ ftplugin/javascriptreact.lua
@@ -3,15 +3,6 @@ if not status_ok then
   return
 end
 
-local opts = {
-  mode = "n", -- NORMAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}
-
 local mappings = {
   C = {
     name = "Javascriptreact",
@@ -24,5 +15,5 @@ local mappings = {
   },
 }
 
+local opts = require('traap.constants').normal_opts
 which_key.register(mappings, opts)
-
