b19d7ab33c33d015a3ad29c6f10bde81ec55d398
Traap <gary.a.howard@mac.com>
2022-11-27 22:24:10 -0800

    continue dotnet dbg.

--- lua/traap/lsp/languages/cs.lua
+++ lua/traap/lsp/languages/cs.lua
@@ -4,17 +4,23 @@ if not ok then return end
 -- // https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Dotnet
 dap.adapters.coreclr = {
   type = 'executable',
-  command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/netcorebg',
-  args = {'--interpreter=vscode'}
+  command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/netcoredbg/netcoredbg',
+  args = {'--interpreter=cli'}
 }
+
+dap.set_log_level('TRACE')
 
 dap.configurations.cs = {
   {
     type = "coreclr",
-    name = "launch - netcoredbg",
+    name = "Launch file",
     request = "launch",
     program = function()
-        return vim.fn.input(os.getenv('HOME') .. '.local/share/nvim/packages/netcorebg/build', 'netcorebg', 'netcorebg')
+      return vim.fn.input(
+        'Path to dll',
+        vim.fn.getcwd() .. '/bin/Debug/net7.0',
+        'file'
+      )
     end,
   },
 }
