be7afadd6a2c3fc3a71a93d97cb8798753c5317d
Traap <gary.a.howard@mac.com>
2022-11-29 07:09:23 -0800

    Setup Ruby dap.

--- lua/traap/lsp/languages/cs.lua
+++ lua/traap/lsp/languages/cs.lua
@@ -2,10 +2,15 @@ local  ok, dap = pcall(require, 'dap')
 if not ok then return end
 
 -- // https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Dotnet
+
+--[[ local command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/netcoredbg/netcoredbg' ]]
+local command = 'netcoredbg'
+local dbgdll = vim.fn.getcwd() .. '/bin/Debug/net7.0/'
+
 dap.adapters.coreclr = {
   type = 'executable',
-  command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/netcoredbg/netcoredbg',
-  args = {'--interpreter=cli'}
+  command = command,
+  args = {'--interpreter=vscode'}
 }
 
 dap.set_log_level('TRACE')
@@ -16,11 +21,7 @@ dap.configurations.cs = {
     name = "Launch file",
     request = "launch",
     program = function()
-      return vim.fn.input(
-        'Path to dll',
-        vim.fn.getcwd() .. '/bin/Debug/net7.0',
-        'file'
-      )
+      return vim.fn.input('Path to dll', dbgdll, 'file')
     end,
   },
 }
