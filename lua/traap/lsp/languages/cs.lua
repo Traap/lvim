8e9e909d19be69cb709d748ec607512ab1b97e14
Traap <gary.a.howard@mac.com>
2022-11-29 23:42:54 -0800

    csharp netcoredbg dap configuration.

--- lua/traap/lsp/languages/cs.lua
+++ lua/traap/lsp/languages/cs.lua
@@ -1,27 +1,21 @@
+-- // https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Dotnet
+
 local  ok, dap = pcall(require, 'dap')
 if not ok then return end
 
--- // https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Dotnet
-
---[[ local command = os.getenv('HOME') .. '/.local/share/nvim/mason/packages/netcoredbg/netcoredbg' ]]
-local command = 'netcoredbg'
-local dbgdll = vim.fn.getcwd() .. '/bin/Debug/net7.0/'
-
 dap.adapters.coreclr = {
   type = 'executable',
-  command = command,
+  command = 'netcoredbg',
   args = {'--interpreter=vscode'}
 }
 
-dap.set_log_level('TRACE')
-
 dap.configurations.cs = {
   {
-    type = "coreclr",
-    name = "Launch file",
-    request = "launch",
+    type = 'coreclr',
+    request = 'launch',
+    name = 'Launch file',
     program = function()
-      return vim.fn.input('Path to dll', dbgdll, 'file')
+       return vim.fn.input('Dbg:', vim.fn.getcwd() .. '/bin/Debug/net7.0/', 'file')
     end,
   },
 }
