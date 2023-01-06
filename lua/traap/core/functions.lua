bd72ec6fe745edf44e3a6af8576d337eb0b91686
Traap <gary.a.howard@mac.com>
2023-01-06 00:06:35 -0800

    Add function keymap_buffer.
    This function maps keys to the current buffer.

--- lua/traap/core/functions.lua
+++ lua/traap/core/functions.lua
@@ -1,3 +1,4 @@
+
 local M = {}
 
 function M.keymap(mode, lhs, rhs, opts)
@@ -6,4 +7,9 @@ function M.keymap(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
+function M.keymap_buffer(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true, buffer = true }
+  options = vim.tbl_deep_extend('force', options, opts or {})
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 return M
